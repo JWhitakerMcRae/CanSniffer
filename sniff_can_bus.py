26875980d2f107ce70f4a10c4d165a2acf86e304
Whitaker McRae <whitaker.mcrae@hivizleds.com>
2020-09-24 16:31:41 -0400

    Formatting, comments, and wording changes

--- sniff_can_bus.py
+++ sniff_can_bus.py
@@ -37,34 +37,36 @@ if __name__ == "__main__":
     parser.add_argument("-b", "--baud", type=int, help="BAUD rate of CAN bus")
     parser.add_argument("-l", "--length", type=int, help="length to run, in seconds (default: infinite)", default=0) 
     args = parser.parse_args()
-    # Bring CAN0 up
+    # Bring up CAN0 interface
     can_up(baud=args.baud)
     try:
         bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
     except OSError:
         print('\n\rCannot find PiCAN board!')
         sys.exit(os.EX_UNAVAILABLE)
-    # Sniff loop
+    # Sniff loop (infinite or until --length seconds)
     start_time = time.time()
     running = True
     try:
+        print("CAN data:")
         while running:
-            message = bus.recv()    # wait until a message is received.
-            c = '{0:f} {1:x} {2:x} '.format(message.timestamp, message.arbitration_id, message.dlc)
-            s=''
+            message = bus.recv()    # wait until a message is received
+            c = "{0:f} {1:x} {2:x} ".format(message.timestamp, message.arbitration_id, message.dlc)
+            s = ""
             for i in range(message.dlc ):
-                s +=  '{0:x} '.format(message.data[i])
-            print(' {}'.format(c+s))
+                s +=  "{0:x} ".format(message.data[i])
+            print(" {}".format(c+s))
             # Check for end of capture period (if set)
-            if args.length:
+            if args.length: # defaults to 0, treated as infinite
                 if time.time() - start_time >= args.length:
-                    print("\n\rReached end of capture period.")
+                    print("\n\rReached end of {} second capture period.".format(args.length))
                     running = False
     except KeyboardInterrupt:
-        print('\n\rKeyboard interrupt detected.')
+        print("\n\rKeyboard interrupt detected.")
     except BaseException as err:
         print("\n\rCaugh unknown exception: {}".format(err))
         can_down()
         sys.exit(os.EX_PROTOCOL)
+    # Bring down CAN0 interface before exit
     can_down()
     sys.exit(os.EX_OK)
